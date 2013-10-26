b8c158fdef0d374a794912cca1c73e62c75da778
Giasone Salati <giasone@mac.com>
2013-10-26 14:25:46 +0100

    installed new tweeter app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweeter::Application.config.secret_key_base = '81c59a7bb6cfdd6f2de80117339019d160b458874053ca86eed4eb1a740f84145f613b834eb55b02cab10cfab6f4232ea5ed6e03377eb8febefc9653528a94a0'
