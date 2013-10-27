d8540bce0361c4fa5882a6fc8261fec3731e58f9
thegias <giasone@mac.com>
2013-10-27 16:18:29 +0000

    added gravatar and CSS

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ ruby '2.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 gem 'devise'
-
+gem 'gravtastic'
 gem 'bootstrap-sass', '~>3.0.0.0.rc'
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tweets
+  
+  include Gravtastic
+  gravtastic
 end
